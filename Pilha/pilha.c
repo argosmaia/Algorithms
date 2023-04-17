6af1238ec1bc4398e650e28fbcd3cebeac4abad3
Argos Maia <33440266+argosmaia@users.noreply.github.com>
2023-04-17 12:17:53 -0300

    Update pilha.c

--- Pilha/pilha.c
+++ Pilha/pilha.c
@@ -1,1 +1,35 @@
 
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 10
+
+int topo = -1; int vet[MAX];
+
+void Empilha();
+void Desempilha();
+void Mostra();
+
+void Empilha()
+{
+    int num;
+    if(topo == MAX - 1)
+    {
+        printf("\nPilha entupida\n");
+    }else{
+        printf("\nDigite um elemento: ");
+        scanf("%d", &num);
+        topo += 1;
+        vet[topo] = num;
+    }
+}
+
+void Desempilha()
+{
+    if(topo==-1)
+    {
+        printf("\nPilha vazia\n");
+    }else{
+		printf("\nExcluir topo:  %d", vet[topo]);
+		topo -= 1;
+    }
+}
