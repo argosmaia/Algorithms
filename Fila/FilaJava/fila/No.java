fed772add2ea57c903e6b5daa3f3f1ef51b4315a
Argos Maia <33440266+argosmaia@users.noreply.github.com>
2023-07-25 19:59:32 -0300

    Update No.java

--- Fila/FilaJava/fila/No.java
+++ Fila/FilaJava/fila/No.java
@@ -37,9 +37,7 @@ public class No<T> {
 /*public class No {
 	private Object object;
 	private No refNo;
-	/**
-	 * @param object
-	 */
+	
 	public No(Object object) {
 		this.object = object;
 		this.refNo = refNo;
@@ -49,30 +47,22 @@ public class No<T> {
 		
 	}
 
-	/**
-	 * @return the object
-	 */
+	
 	public Object getObject() {
 		return object;
 	}
 
-	/**
-	 * @param object the object to set
-	 */
+	
 	public void setObject(Object object) {
 		this.object = object;
 	}
 
-	/**
-	 * @return the refNo
-	 */
+	
 	public No getRefNo() {
 		return refNo;
 	}
 
-	/**
-	 * @param refNo the refNo to set
-	 */
+	
 	public void setRefNo(No refNo) {
 		this.refNo = refNo;
 	}
